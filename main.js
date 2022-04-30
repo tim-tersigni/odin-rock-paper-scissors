cf7d6f278693df438ed3aa0afd82741e58c07014
Tim Tersigni <tim.tersigni@gmail.com>
2022-04-30 17:16:11 -0500

    playRound() returns result of a round given player and computer input

--- main.js
+++ main.js
@@ -4,4 +4,38 @@ function computerPlay() {
     return options[rand]
 }
 
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection) {
+    const options = ["Rock", "Paper", "Scissors"]
+
+    // Assign values based on list position
+    let playerValue
+    let computerValue
+    let val = 0
+    for(let o of options) {
+        if(playerSelection == o) {
+            playerValue = val
+        }
+
+        if(computerSelection == o) {
+            computerValue = val
+        }
+        val += 1
+    }
+
+    // Determine result based off of list position values
+    let result = playerValue - computerValue
+    if(result < 0) {
+        result += 3
+    }
+
+    // Return outcome
+    if(result % 2 > 0) {
+        return `You Win! ${playerSelection} beats ${computerSelection}.`
+    }
+    else if (result == 0) {
+        return `You tie! Both picked ${playerSelection}.`
+    }
+    else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}.`
+    }
+}
